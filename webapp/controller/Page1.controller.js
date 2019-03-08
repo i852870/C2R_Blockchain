2cbff131045df33410529885b5132ba267f3ce4d
Barot Purav <purav.barot@sap.com>
2019-03-08 21:15:04 +0000

    first changes
    Change-Id: I6e11fc775b373e7c3ba1ace72e94c3682d62a859

---
+++ webapp/controller/Page1.controller.js
@@ -0,0 +1,67 @@
+sap.ui.define(["sap/ui/core/mvc/Controller",
+	"sap/m/MessageBox",
+	"./utilities",
+	"sap/ui/core/routing/History"
+], function(BaseController, MessageBox, Utilities, History) {
+	"use strict";
+
+	return BaseController.extend("com.sap.build.standard.test.controller.Page1", {
+		handleRouteMatched: function(oEvent) {
+			var sAppId = "App5c8175d3d57c156011897813";
+
+			var oParams = {};
+
+			if (oEvent.mParameters.data.context) {
+				this.sContext = oEvent.mParameters.data.context;
+
+			} else {
+				if (this.getOwnerComponent().getComponentData()) {
+					var patternConvert = function(oParam) {
+						if (Object.keys(oParam).length !== 0) {
+							for (var prop in oParam) {
+								if (prop !== "sourcePrototype") {
+									return prop + "(" + oParam[prop][0] + ")";
+								}
+							}
+						}
+					};
+
+					this.sContext = patternConvert(this.getOwnerComponent().getComponentData().startupParameters);
+
+				}
+			}
+
+			var oPath;
+
+			if (this.sContext) {
+				oPath = {
+					path: "/" + this.sContext,
+					parameters: oParams
+				};
+				this.getView().bindObject(oPath);
+			}
+
+		},
+		_onFileUploaderUploadComplete: function() {
+			// Please implement
+
+		},
+		_onFileUploaderChange: function() {
+			// Please implement
+
+		},
+		_onFileUploaderTypeMissmatch: function() {
+			// Please implement
+
+		},
+		_onFileUploaderFileSizeExceed: function() {
+			// Please implement
+
+		},
+		onInit: function() {
+			this.oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+			this.oRouter.getTarget("Page1").attachDisplay(jQuery.proxy(this.handleRouteMatched, this));
+
+		}
+	});
+}, /* bExport= */ true);
